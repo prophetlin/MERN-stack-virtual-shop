df73efbe487985041a2c53969713f43634000774
William Lin <42307232+prophetlin@users.noreply.github.com>
2022-05-19 07:44:41 +1000

    update seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -17,17 +17,21 @@ const importData = async () => {
     await Order.deleteMany()
     await Product.deleteMany()
     await User.deleteMany()
+
     const createdUsers = await User.insertMany(users)
+
     const adminUser = createdUsers[0]._id
+
     const sampleProducts = products.map((product) => {
       return { ...product, user: adminUser }
     })
 
     await Product.insertMany(sampleProducts)
 
-    console.log('Data Imported'.green.inverse)
+    console.log('Data Imported!'.green.inverse)
+    process.exit()
   } catch (error) {
-    console.error(`${error}`)
+    console.error(`${error}`.red.inverse)
     process.exit(1)
   }
 }
@@ -38,15 +42,15 @@ const destroyData = async () => {
     await Product.deleteMany()
     await User.deleteMany()
 
-    console.log('Data Destroyed'.red.inverse)
+    console.log('Data Destroyed!'.red.inverse)
     process.exit()
   } catch (error) {
-    console.error(`${error}`)
+    console.error(`${error}`.red.inverse)
     process.exit(1)
   }
 }
 
-if (process.argv[2] == 'd') {
+if (process.argv[2] === '-d') {
   destroyData()
 } else {
   importData()
