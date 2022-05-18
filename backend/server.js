d2e1fec366bb3a1645ec7334210005ccf3b67693
William Lin <42307232+prophetlin@users.noreply.github.com>
2022-05-18 16:46:48 +1000

    update server

--- backend/server.js
+++ backend/server.js
@@ -13,4 +13,4 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
-app.listen(5000, console.log('Server running on port 5000'))
+app.listen(5000, console.log('Server is running on port 5000'))
